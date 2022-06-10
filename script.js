119322969fcae29338303969611d8e83fa4a1fd5
Jean-Pierre Graham <91097729+Goshufit@users.noreply.github.com>
2022-06-09 21:14:46 -0400

    Add childNodes, parenNode, parentElement, children, firstChild, firstElementChild

---
+++ script.js
@@ -0,0 +1,41 @@
+//parentNode//
+
+// let itemList = document.querySelector('#items');
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentNode.parentNode.parentNode);
+
+
+//parentElement//
+
+// let itemList = document.querySelector('#items');
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentElement.parentElement.parentElement);
+
+
+//childNodes//
+
+// let itemList = document.querySelector('#items');
+// console.log(itemList.childNodes);
+//When selecting nodes with the childNode selector it also selects the white space in between the elements so instead use the 'children' selector//
+
+
+//children//
+
+// let itemList = document.querySelector('#items');
+// console.log(itemList.children);
+// console.log(itemList.children[1]);
+// itemList.children[1].style.backgroundColor = 'yellow';
+
+
+//firstChild//
+
+// let itemList = document.querySelector('#items');
+// console.log(itemList.firstChild);
+//This is similar to child nodes where it will select white space before the desired element and lod it first//
+
+
+//firstElementChild//
+
+let itemList = document.querySelector('#items');
