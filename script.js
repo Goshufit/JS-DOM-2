96ca54a7df0513e98d033a2b82d527e7fd74d79d
Jean-Pierre Graham <91097729+Goshufit@users.noreply.github.com>
2022-06-10 23:58:32 -0400

    Add fistChild, lastChile,nextElementSibling, previousElement sibling and Create elements

--- script.js
+++ script.js
@@ -33,9 +33,75 @@
 
 // let itemList = document.querySelector('#items');
 // console.log(itemList.firstChild);
-//This is similar to child nodes where it will select white space before the desired element and lod it first//
+//This is similar to childNodes where it will select white space before the desired element and lod it first//
 
 
 //firstElementChild//
 
-let itemList = document.querySelector('#items');
+// let itemList = document.querySelector('#items');
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent = 'Hello 1';
+
+
+//lastChild//
+
+// let itemList = document.querySelector('#items');
+// console.log(itemList.lastChild);
+//This is similar to childNodes where it will select white space before the desired element and lod it first//
+
+
+//lastElementChild//
+
+// let itemList = document.querySelector('#items');
+// console.log(itemList.lastElementChild);
+// itemList.lastElementChild.textContent = 'Hello 4';
+
+
+//nextSibling//
+
+// let itemList = document.querySelector('#items');
+// console.log(itemList.nextSibling);
+
+
+//nextElementSibling//
+
+// let itemList = document.querySelector('#items');
+// console.log(itemList.nextElementSibling);
+
+
+//previousSibling//
+
+// let itemList = document.querySelector('#items');
+// console.log(itemList.previousSibling);
+//This is similar to childNodes where it will select white space before the desired element and lod it first//
+
+
+//previousElementSibling//
+// let itemList = document.querySelector('#items');
+// console.log(itemList.previousElementSibling);
+// itemList.previousElementSibling.style.color = 'green';
+
+/////////////////////////////////////////////////////////////////////////////
+//CREATING ELEMENTS//
+
+
+//createElement//
+//create div//
+
+let newDiv = document.createElement('div');
+//Add class
+newDiv.className = 'hello';
+//Add ID
+newDiv.id = 'hello1';
+//Add attribute
+newDiv.setAttribute('title', 'Hello Div');
+//Create text node
+let newDivText = document.createTextNode('Hello World');
+//Add text to div
+newDiv.appendChild(newDivText);
+
+let container = document.querySelector('header .container')
+let h1 = document.querySelector('header h1');
+console.log(newDiv);
+newDiv.style.fontSize = '30px';
+container.insertBefore(newDiv, h1);
